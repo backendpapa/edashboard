74827ba85359d7bb25a0f53505995ebbccade4af
Akeju Paul <akejupaul10@gmail.com>
2021-08-25 18:37:47 +0100

    make it better

--- src/components/Courses/AllCourses.vue
+++ src/components/Courses/AllCourses.vue
@@ -9,6 +9,15 @@
               <p class="mt-n5 text-caption">By Backendpapa</p>
           </div>
           <v-spacer></v-spacer>
+          <div class="mt-3">
+            <p><v-icon size="17px">mdi-clock</v-icon><span class="text-caption font-weight-bold ml-1">6hr 30min</span></p>
+          </div>
+          <v-spacer></v-spacer>
+          <div class="mt-3">
+              <p class="mt-2"><v-icon class="mt-n2 mr-2">mdi-fire</v-icon> <span class="font-weight-bold ">4.9</span></p>
+          </div>
+          <v-spacer></v-spacer>
+          <v-btn color="black" dark style="text-transform:none" class="text-caption mr-2">VIew Course</v-btn>
       </v-row>
   </div>
 </template>
