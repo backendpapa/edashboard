a546110ab7f10eddfb5febedc0ca56360c507d89
Akeju Paul <akejupaul10@gmail.com>
2021-08-29 02:27:11 +0100

    make it better

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Courses from '../views/Courses.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -9,6 +10,11 @@ const routes = [
     path: '/course/overview',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/course/courses',
+    name: 'Courses',
+    component: Courses
   },
   {
     path: '/',
---
+++ src/views/Courses.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <BottomBar />
+  </div>
+</template>
+
+<script>
+import BottomBar from '../components/BottomBar.vue'
+export default {
+    components:{
+        BottomBar
+    }
+}
+</script>
+
+<style>
+
+</style>
